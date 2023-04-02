44a58c35b5f1bb108461465be0c8b0ddc3cfc9ea
ivangmg <ivangmg@hotmail.com>
2023-04-01 19:29:50 -0700

    form with input bug

--- src/utils/form/Form.js
+++ src/utils/form/Form.js
@@ -8,31 +8,46 @@ function Form() {
   const [message, setMessage] = useState('')
   const [fieldRequiredError, setFieldRequiredError] = useState('')
   const [showError, setShowError] = useState(false);
-  // const handleInputChange = (e) => {
-  //   setShowError(false);
-  //   // Getting the value and name of the input which triggered the change
-  //   const { name, value } = e.target;
-  //   if (value === "" && name === "firstName") {
-  //     setFirstNameError("Not empty values allowed")
-  //   }
-  //   if (value === "" && name === "lastName") {
-  //     setLastNameError("Not empty values allowed")
-  //   }
+  const handleInputChange = (e) => {
+    setShowError(false);
+    // Getting the value and name of the input which triggered the change
+    const { name, eMail, message, value } = e.target;
 
-  //   if (value !== "" && name === "firstName") {
-  //     setFirstNameError("")
-  //   }
-  //   if (value !== "" && name === "lastName") {
-  //     setLastNameError("")
-  //   }
-  //   // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
-  //   return name === 'firstName' ? setFirstName(value) : setLastName(value);
-  // };
+    if (value === "" && name === "name") {
+      setFieldRequiredError("Not empty values allowed")
+    }
+    if (value === "" && eMail === "email") {
+      setFieldRequiredError("Not empty values allowed")
+    }
+    if (value === "" && message === "message") {
+      setFieldRequiredError("Not empty values allowed")
+    }
+    if (value !== "" && name === "name") {
+      setFieldRequiredError("")
+    }
+    if (value !== "" && eMail === "email") {
+      setFieldRequiredError("")
+    }
+    if (value !== "" && message === "message") {
+      setFieldRequiredError("")
+    }
+    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+    // return name === 'name' ? setName(value) : setEmail(value) : setMessage (avalue);
+    if (name === "name" || eMail === "" || message === "") {
+      return setName(value);
+    }
+    if (name === "" || eMail === "email" || message === "") {
+      return setEmail(value);
+    }
+    if (name === "" || eMail === "" || message === "message") {
+      return setMessage(value);
+    }
+  };
 
   const handleFormSubmit = (e) => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
-    const { name, eMail, message, value } = e.target;
+  
     if (name === "") {
       setFieldRequiredError ("Not empty values allowed")
     }
@@ -63,21 +78,21 @@ function Form() {
         <input
           value={name}
           name="name"
-          // onChange={handleInputChange}
+          onChange={handleInputChange}
           type="text"
         />
         <p>Email</p>
         <input
           value={eMail}
           name="email"
-          // onChange={handleInputChange}
+          onChange={handleInputChange}
           type="text"
         />
         <p>Message</p>
         <input
           value={message}
           name="message"
-          // onChange={handleInputChange}
+          onChange={handleInputChange}
           type="text"
         />
         <button type="button" onClick={handleFormSubmit}>
