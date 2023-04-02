f651479b3e4970f2335316a44d3f96d711342874
ivangmg <ivangmg@hotmail.com>
2023-04-02 14:12:48 -0700

    footer links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
     console.log("Footer component rendered")
     return <footer>
-        <p>Copyright {new Date().getFullYear()}</p>
+        <a href="https://github.com/ivangmg83" class="link">GitHub</a><br/>
+        <a href="https://www.linkedin.com/in/ivan-munoz-507152140/" class="link">LinkedIn</a><br/>
     </footer>
 }
 
